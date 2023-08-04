83604f5466e8162bea7ca7893605c99df2fc1830
Jared Black <jaredblackjcb@gmail.com>
2023-08-04 07:43:46 -0500

    Access state for message context

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -1,42 +1,25 @@
-import React from "react";
-// import axios from "axios";
+class ActionProvider {
+  constructor(createChatBotMessage, setStateFunc) {
+    this.createChatBotMessage = createChatBotMessage;
+    this.setState = setStateFunc;
+  }
 
-const ActionProvider = ({ createChatBotMessage, setState, children }) => {
-  const mockResponse = () => {
-    const response = "response message";
-    return response;
+  handleAiMessage = (aiMessage) => {
+    const message = this.createChatBotMessage(aiMessage, {
+      loading: true,
+      terminateLoading: true,
+      withAvatar: true,
+    });
+
+    this.addMessageToState(message);
   };
 
-  const getBotMessage = (prevMessages) => {
-    // Send previous messages to API endpoint and retrieve the new message
-    console.log(prevMessages);
-    try {
-      const response = mockResponse("mock AI message"); //await axios.post("api/v1/chat", prevRef.current);
-      // Bot will return response in the message property of the response
-      return response;
-    } catch (e) {
-      return `I'm sorry, I seem to be broken right now. Please try again later.`;
-    }
-  };
-
-  const handleAiMessage = async (message) => {
-    setState((prev) => ({
-      ...prev,
-      messages: [...prev.messages, createChatBotMessage(getBotMessage(prev))],
+  addMessageToState = (message) => {
+    this.setState((state) => ({
+      ...state,
+      messages: [...state.messages, message],
     }));
   };
-
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          actions: {
-            handleAiMessage,
-          },
-        });
-      })}
-    </div>
-  );
-};
+}
 
 export default ActionProvider;
--- src/chatbot/MessageParser.js
+++ src/chatbot/MessageParser.js
@@ -1,21 +1,37 @@
-import React from "react";
+import axios from "axios";
+class MessageParser {
+  constructor(actionProvider, state) {
+    this.actionProvider = actionProvider;
+    this.state = state;
+  }
 
-const MessageParser = ({ children, actions }) => {
-  const parse = (message) => {
-    // TODO: Send message to backend to handle parsing using AI
-    actions.handleAiMessage(message);
-  };
+  createBody(message) {
+    const context = this.state.messages.map(({ message, type }) => ({ message, type }));
+    const body = {
+      message: message,
+      context: context,
+    };
 
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          parse: parse,
-          actions: {},
-        });
-      })}
-    </div>
-  );
-};
+    return JSON.stringify(body);
+  }
+
+  getAiMessage(message) {
+    const body = this.createBody(message);
+    console.log(body);
+    try {
+      const response = "test response"; // await axios.get("/api/v1/chat", body);
+      return response;
+    } catch (e) {
+      return "I'm sorry, I seem to be broken right now. Please try again later.";
+    }
+  }
+
+  parse(message) {
+    console.log(this.state);
+    console.log(message);
+    const newMessage = this.getAiMessage(message);
+    return this.actionProvider.handleAiMessage(newMessage);
+  }
+}
 
 export default MessageParser;
